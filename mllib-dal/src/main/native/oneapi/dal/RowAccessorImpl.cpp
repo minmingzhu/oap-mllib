534309d5a20e91afe1e4a1ea0a9c90aea3a08410
minmingzhu <minming.zhu@intel.com>
2022-04-17 13:14:19 +0800

    1. convert homogenTable to array/vector/matrix 2. add unit test
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/oneapi/dal/RowAccessorImpl.cpp
+++ mllib-dal/src/main/native/oneapi/dal/RowAccessorImpl.cpp
@@ -25,7 +25,6 @@
 #ifdef CPU_GPU_PROFILE
 #include "DPCPPGPU.h"
 #endif
-
 #ifndef ONEDAL_DATA_PARALLEL
 #define ONEDAL_DATA_PARALLEL
 #endif
@@ -38,7 +37,6 @@
 using namespace std;
 using namespace oneapi::dal;
 typedef std::shared_ptr<homogen_table> homogenPtr;
-
 
 /*
  * Class:     com_intel_oneapi_dal_table_RowAccessor
