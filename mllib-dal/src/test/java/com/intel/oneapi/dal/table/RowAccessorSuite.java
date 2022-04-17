7ff9cf1e44d8b8db6b28bc2971b9424a1fa83965
minmingzhu <minming.zhu@intel.com>
2022-04-17 13:14:19 +0800

    1. convert homogenTable to array/vector/matrix 2. add unit test
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/test/java/com/intel/oneapi/dal/table/RowAccessorSuite.java
+++ mllib-dal/src/test/java/com/intel/oneapi/dal/table/RowAccessorSuite.java
@@ -5,6 +5,7 @@ package com.intel.oneapi.dal.table;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -25,10 +26,14 @@ import org.junit.Test;
 =======
 import org.junit.jupiter.api.Test;
 >>>>>>> rollback to edb9f3d
+=======
+import org.junit.Test;
+>>>>>>> 1. convert homogenTable to array/vector/matrix
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+<<<<<<< HEAD
 import static com.intel.oneapi.dal.table.Common.DataLayout.ROW_MAJOR;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,12 +53,21 @@ public class RowAccessorSuite {
 >>>>>>> 1. convert homogenTable to array/vector/matrix
 =======
 >>>>>>> rollback to edb9f3d
+=======
+import static com.intel.oneapi.dal.table.Common.DataLayout.ROWMAJOR;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class RowAccessorSuite {
+    private static final double MAXIMUMDOUBLEDELTA = 0.000001d;
+>>>>>>> 1. convert homogenTable to array/vector/matrix
 
     @Test
     public void readTableDataFromRowAccessor() {
         double[] data = {5.236359d, 8.718667d, 40.724176d, 10.770023d, 90.119887d, 3.815366d,
                 53.620204d, 33.219769d, 85.208661d, 15.966239d};
         HomogenTable table = new HomogenTable(5, 2,
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -93,10 +107,14 @@ public class RowAccessorSuite {
 =======
                 data, ROW_MAJOR, CommonTest.getDevice());
 >>>>>>> rollback to edb9f3d
+=======
+                data, Double.class, ROWMAJOR.ordinal());
+>>>>>>> 1. convert homogenTable to array/vector/matrix
 
         RowAccessor accessor = new RowAccessor(table.getcObejct());
         double[] rowData = accessor.pullDouble( 0 , table.getRowCount());
         assertEquals(new Long(rowData.length), new Long(table.getColumnCount() * table.getRowCount()));
+<<<<<<< HEAD
         assertArrayEquals(rowData, data);
         for (int i = 0; i < rowData.length; i++) {
 <<<<<<< HEAD
@@ -114,6 +132,11 @@ public class RowAccessorSuite {
 =======
             assertEquals(rowData[i], data[i]);
 >>>>>>> rollback to edb9f3d
+=======
+        assertArrayEquals("", rowData, data, MAXIMUMDOUBLEDELTA);
+        for (int i = 0; i < rowData.length; i++) {
+            assertEquals(rowData[i], data[i], MAXIMUMDOUBLEDELTA);
+>>>>>>> 1. convert homogenTable to array/vector/matrix
         }
     }
 }
