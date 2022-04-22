d952d0cdb428554c29f61055ef08e9272b88dfac
minmingzhu <minming.zhu@intel.com>
2022-04-22 10:58:36 +0800

    add empty homogentable
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/oneapi/dal/HomogenTableImpl.cpp
+++ mllib-dal/src/main/native/oneapi/dal/HomogenTableImpl.cpp
@@ -183,8 +183,6 @@ JNIEXPORT jlong JNICALL Java_com_intel_oneapi_dal_table_HomogenTableImpl_dInit(
     JNIEnv *env, jobject, jlong cRowCount, jlong cColCount, jdoubleArray cData,
     jint cLayout, jint cComputeDevice) {
     printf("HomogenTable double init \n");
-    std::cout << "jdoubleArray " << cData
-                      << std::endl;
     jdouble *fData = env->GetDoubleArrayElements(cData, NULL);
     homogen_table *h_table ;
     homogenPtr tablePtr ;
@@ -430,7 +428,8 @@ Java_com_intel_oneapi_dal_table_HomogenTableImpl_cGetDoubleData(
 JNIEXPORT jlong JNICALL Java_com_intel_oneapi_dal_table_HomogenTableImpl_cEmptyTableInit
   (JNIEnv *env, jobject) {
       printf(" init empty HomogenTable \n");
-      homogen_table *h_table = nullptr;
+      homogen_table *h_table = new homogen_table();
+
       std::shared_ptr<homogen_table> *tablePtr =
               new std::shared_ptr<homogen_table>(h_table);
       cVector.push_back(*tablePtr);
