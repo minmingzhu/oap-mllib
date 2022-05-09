0ea2448d4a977ce6093c97643b97eb027423294b
minmingzhu <minming.zhu@intel.com>
2022-05-09 17:00:14 +0800

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/java/com/intel/oneapi/dal/table/ColumnAccessor.java
+++ mllib-dal/src/main/java/com/intel/oneapi/dal/table/ColumnAccessor.java
@@ -7,43 +7,47 @@ public class ColumnAccessor {
         this.cObject = cObject;
     }
 
-    public double[] pullDouble(long columnIndex){
-        return this.cPullDouble(this.cObject, columnIndex, 0, -1);
+    public double[] pullDouble(long columnIndex, Common.ComputeDevice device){
+        return this.cPullDouble(this.cObject, columnIndex, 0, -1, device.ordinal());
     }
 
-    public double[] pullDouble(long columnIndex, long rowStartIndex, long rowEndIndex){
-        return this.cPullDouble(this.cObject, columnIndex, rowStartIndex, rowEndIndex);
+    public double[] pullDouble(long columnIndex, long rowStartIndex, long rowEndIndex, Common.ComputeDevice device){
+        return this.cPullDouble(this.cObject, columnIndex, rowStartIndex, rowEndIndex, device.ordinal());
     }
 
-    private native double[] cPullDouble(long cObject, long cColumnIndex, long cRowStartIndex, long cRowEndIndex);
+    private native double[] cPullDouble(long cObject, long cColumnIndex,
+                                        long cRowStartIndex, long cRowEndIndex, int computeDeviceIndex);
 
-    public float[] pullFloat(long columnIndex){
-        return this.cPullFloat(this.cObject, columnIndex, 0, -1);
+    public float[] pullFloat(long columnIndex, int computeDeviceIndex){
+        return this.cPullFloat(this.cObject, columnIndex, 0, -1, computeDeviceIndex);
     }
 
-    public float[] pullFloat(long columnIndex, long rowStartIndex, long rowEndIndex){
-        return this.cPullFloat(this.cObject, columnIndex, rowStartIndex, rowEndIndex);
+    public float[] pullFloat(long columnIndex, long rowStartIndex, long rowEndIndex, Common.ComputeDevice device){
+        return this.cPullFloat(this.cObject, columnIndex, rowStartIndex, rowEndIndex, device.ordinal());
     }
 
-    private native float[] cPullFloat(long cObject, long cColumnIndex, long cRowStartIndex, long cRowEndIndex);
+    private native float[] cPullFloat(long cObject, long cColumnIndex, long cRowStartIndex,
+                                      long cRowEndIndex, int computeDeviceIndex);
 
-    public int[] pullInt(long columnIndex){
-        return this.cPullInt(this.cObject, columnIndex, 0, -1);
+    public int[] pullInt(long columnIndex, Common.ComputeDevice device){
+        return this.cPullInt(this.cObject, columnIndex, 0, -1, device.ordinal());
     }
 
-    public int[] pullInt(long columnIndex, long rowStartIndex, long rowEndIndex){
-        return this.cPullInt(this.cObject, columnIndex, rowStartIndex, rowEndIndex);
+    public int[] pullInt(long columnIndex, long rowStartIndex, long rowEndIndex, Common.ComputeDevice device){
+        return this.cPullInt(this.cObject, columnIndex, rowStartIndex, rowEndIndex, device.ordinal());
     }
 
-    private native int[] cPullInt(long cObject, long cColumnIndex, long cRowStartIndex, long cRowEndIndex);
+    private native int[] cPullInt(long cObject, long cColumnIndex, long cRowStartIndex,
+                                  long cRowEndIndex, int computeDeviceIndex);
 
-    public long[] pullLong(long columnIndex){
-        return this.cPullLong(this.cObject, columnIndex, 0, -1);
+    public long[] pullLong(long columnIndex, Common.ComputeDevice device){
+        return this.cPullLong(this.cObject, columnIndex, 0, -1, device.ordinal());
     }
 
-    public long[] pullLong(long columnIndex, long rowStartIndex, long rowEndIndex){
-        return this.cPullLong(this.cObject, columnIndex, rowStartIndex, rowEndIndex);
+    public long[] pullLong(long columnIndex, long rowStartIndex, long rowEndIndex, Common.ComputeDevice device){
+        return this.cPullLong(this.cObject, columnIndex, rowStartIndex, rowEndIndex, device.ordinal());
     }
 
-    private native long[] cPullLong(long cObject, long cColumnIndex, long cRowStartIndex, long cRowEndIndex);
+    private native long[] cPullLong(long cObject, long cColumnIndex, long cRowStartIndex,
+                                    long cRowEndIndex, int computeDeviceIndex);
 }
--- mllib-dal/src/main/native/oneapi/dal/ColumnAccessorImpl.cpp
+++ mllib-dal/src/main/native/oneapi/dal/ColumnAccessorImpl.cpp
@@ -32,6 +32,7 @@
 #include "com_intel_oneapi_dal_table_ColumnAccessor.h"
 #include "oneapi/dal/table/homogen.hpp"
 #include "oneapi/dal/table/column_accessor.hpp"
+#include "service.h"
 
 using namespace std;
 using namespace oneapi::dal;
@@ -42,27 +43,51 @@ using namespace oneapi::dal;
  * Signature: (JJ)[D
  */
 JNIEXPORT jdoubleArray JNICALL Java_com_intel_oneapi_dal_table_ColumnAccessor_cPullDouble
-  (JNIEnv *env, jobject, jlong cTableAddr, jlong cColumnIndex, jlong cRowStartIndex, jlong cRowEndIndex) {
-      homogen_table *htable =
-              ((std::shared_ptr<homogen_table> *)cTableAddr)->get();
-      column_accessor<const double> *acc = new column_accessor<const double>(*htable);
-      #ifdef CPU_GPU_PROFILE
-          sycl::queue queue = getQueue();
-          const auto sycl_col_values = acc->pull(cColumnIndex, {cRowStartIndex, cRowEndIndex});
-          double* sycl_col_doubles = new double[sycl_col_values.get_count()];
-          for (std::int64_t i = 0; i < sycl_col_values.get_count(); i++) {
-              sycl_col_doubles[i] = sycl_col_values[i];
-          }
-          jdoubleArray sycl_newDoubleArray = env->NewDoubleArray(sycl_col_values.get_count());
-          env->SetDoubleArrayRegion(sycl_newDoubleArray, 0, sycl_col_values.get_count(), sycl_col_doubles);
-          return sycl_newDoubleArray;
-      #endif
-          const auto col_values = acc->pull(cColumnIndex, {cRowStartIndex, cRowEndIndex});
-          double* col_doubles = new double[col_values.get_count()];
-          for (std::int64_t i = 0; i < col_values.get_count(); i++) {
-              col_doubles[i] = col_values[i];
-          }
-          jdoubleArray newDoubleArray = env->NewDoubleArray(col_values.get_count());
-          env->SetDoubleArrayRegion(newDoubleArray, 0, col_values.get_count(), col_doubles);
-          return newDoubleArray;
+  (JNIEnv *env, jobject, jlong cTableAddr, jlong cColumnIndex, jlong cRowStartIndex,
+   jlong cRowEndIndex, jint cComputeDevice) {
+  printf("ColumnAccessor PullDouble \n");
+  homogen_table *htable =
+          ((std::shared_ptr<homogen_table> *)cTableAddr)->get();
+  column_accessor<const double> *acc = new column_accessor<const double>(*htable);
+  double* col_doubles;
+  jdoubleArray newDoubleArray;
+  switch(getComputeDevice(cComputeDevice)) {
+       case compute_device::host:{
+              const auto col_values = acc->pull(cColumnIndex, {cRowStartIndex, cRowEndIndex});
+              col_doubles = new double[col_values.get_count()];
+              for (std::int64_t i = 0; i < col_values.get_count(); i++) {
+                  col_doubles[i] = col_values[i];
+              }
+              newDoubleArray = env->NewDoubleArray(col_values.get_count());
+              env->SetDoubleArrayRegion(newDoubleArray, 0, col_values.get_count(), col_doubles);
+              return newDoubleArray;
+       }
+#ifdef CPU_GPU_PROFILE
+       case compute_device::cpu:{
+              sycl::queue *cpu_queue = getQueue(false);
+              const auto cpu_col_values = acc->pull(*cpu_queue, cColumnIndex, {cRowStartIndex, cRowEndIndex});
+              col_doubles = new double[cpu_col_values.get_count()];
+              for (std::int64_t i = 0; i < cpu_col_values.get_count(); i++) {
+                  col_doubles[i] = cpu_col_values[i];
+              }
+              newDoubleArray = env->NewDoubleArray(cpu_col_values.get_count());
+              env->SetDoubleArrayRegion(newDoubleArray, 0, cpu_col_values.get_count(), col_doubles);
+              return newDoubleArray;
+       }
+       case compute_device::gpu:{
+              sycl::queue *gpu_queue = getQueue(true);
+              const auto gpu_col_values = acc->pull(*gpu_queue, cColumnIndex, {cRowStartIndex, cRowEndIndex});
+              col_doubles = new double[gpu_col_values.get_count()];
+              for (std::int64_t i = 0; i < gpu_col_values.get_count(); i++) {
+                  col_doubles[i] = gpu_col_values[i];
+              }
+              newDoubleArray = env->NewDoubleArray(gpu_col_values.get_count());
+              env->SetDoubleArrayRegion(newDoubleArray, 0, gpu_col_values.get_count(), col_doubles);
+              return newDoubleArray;
+       }
+#endif
+       default: {
+             return newDoubleArray;
+       }
+    }
   }
