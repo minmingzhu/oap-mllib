0ad11d61076c33992acfe0b32d68078d3e327103
minmingzhu <minming.zhu@intel.com>
2022-05-09 17:00:14 +0800

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/oneapi/dal/ColumnAccessorImpl.cpp
+++ mllib-dal/src/main/native/oneapi/dal/ColumnAccessorImpl.cpp
@@ -39,6 +39,7 @@ using namespace std;
 using namespace oneapi::dal;
 typedef std::shared_ptr<homogen_table> homogenPtr;
 
+
 /*
  * Class:     com_intel_oneapi_dal_table_ColumnAccessor
  * Method:    cPull
@@ -154,4 +155,3 @@ switch(getComputeDevice(cComputeDevice)) {
     env->SetIntArrayRegion(newIntArray, 0, col_values.get_count(), col_values.get_data());
     return newIntArray;
 }
-
