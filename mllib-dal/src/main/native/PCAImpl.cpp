e18c672bb7264b6a548a94947e86ef93c525be23
minmingzhu <minming.zhu@intel.com>
2023-10-19 07:01:46 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/PCAImpl.cpp
+++ mllib-dal/src/main/native/PCAImpl.cpp
@@ -221,7 +221,7 @@ static void doPCAOneAPICompute(
         const auto pca_desc = descriptor_t().set_deterministic(true);
 
         const auto result_train =
-            preview::compute(queue, pca_desc, result.get_cor_matrix());
+            train(queue, pca_desc, result.get_cor_matrix());
         t2 = std::chrono::high_resolution_clock::now();
         duration = (float)std::chrono::duration_cast<std::chrono::milliseconds>(
                        t2 - t1)
