ae2d8526f6d090672ad062432046e0af78dbaf63
minmingzhu <minming.zhu@intel.com>
2023-10-26 07:47:01 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/PCAImpl.cpp
+++ mllib-dal/src/main/native/PCAImpl.cpp
@@ -23,20 +23,10 @@
 #include "oneapi/dal/algo/pca.hpp"
 #endif
 
-#ifndef ONEDAL_DATA_CONVERSION
-#define ONEDAL_DATA_CONVERSION
-#include "data_management/data_source/csv_feature_manager.h"
-#include "data_management/data_source/file_data_source.h"
-#undef ONEDAL_DATA_CONVERSION
-#endif
-
 #include "Logger.h"
 #include "OneCCL.h"
 #include "com_intel_oap_mllib_feature_PCADALImpl.h"
 #include "service.h"
-#include "oneapi/dal/table/common.hpp"
-#include "oneapi/dal/io/csv.hpp"
-#include "oneapi/dal/table/row_accessor.hpp"
 
 using namespace std;
 #ifdef CPU_GPU_PROFILE
@@ -191,37 +181,6 @@ static void doPCADAALCompute(JNIEnv *env, jobject obj, size_t rankId,
 }
 
 #ifdef CPU_GPU_PROFILE
-std::vector<std::string> get_file_path(const std::string& path) {
-    std::vector<std::string> result;
-    for (auto& file : fs::directory_iterator(path)){
-         if(fs::is_empty(file.path())){
-             continue;
-         }else if(file.path().extension()==".crc" || file.path().extension()==""){
-             continue;
-         }else{
-            result.push_back(file.path());
-         }
-    }
-    return result;
-}
-
-inline bool check_file(const std::string& name) {
-    return std::ifstream{ name }.good();
-}
-
-inline std::string get_data_path(const std::string& name) {
-    const std::vector<std::string> paths = { "./data", "samples/oneapi/dpc/mpi/data" };
-
-    for (const auto& path : paths) {
-        const std::string try_path = path + "/" + name;
-        if (check_file(try_path)) {
-            return try_path;
-        }
-    }
-
-    return name;
-}
-
 static void doPCAOneAPICompute(
     JNIEnv *env, jlong pNumTabData, jlong numRows, jlong numClos,
     preview::spmd::communicator<preview::spmd::device_memory_access::usm> comm,
