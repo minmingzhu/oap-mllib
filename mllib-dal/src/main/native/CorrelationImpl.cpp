965036a7c9771e447008ea699920402ff8d5292a
minmingzhu <minming.zhu@intel.com>
2024-07-29 23:55:31 +0800

    update

--- mllib-dal/src/main/native/CorrelationImpl.cpp
+++ mllib-dal/src/main/native/CorrelationImpl.cpp
@@ -320,9 +320,9 @@ Java_com_intel_oap_mllib_stat_CorrelationDALImpl_cCorrelationTrainDAL(
             rank);
         jint *gpuIndices = env->GetIntArrayElements(gpuIdxArray, 0);
 
-        getGPU(device, gpuIndices);
-        auto gpu_device = sycl::device(sycl::gpu_selector_v);
-        sycl::queue queue{gpu_device};
+        auto queue = getGPU(device, gpuIndices);
+//        auto gpu_device = sycl::device(sycl::gpu_selector_v);
+//        sycl::queue queue{gpu_device};
         const char* cstr = env->GetStringUTFChars(breakdown_name, nullptr);
         std::string c_breakdown_name(cstr);
         const char *str = env->GetStringUTFChars(ip_port, 0);
